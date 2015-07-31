73b8fa57b9b19649615c8bca232db4d68393562f
agroasian <agro_asian@naver.com>
2015-07-31 16:55:21 +0900

    Create coderbyte_.rb

---
+++ coderbyte_.rb
@@ -0,0 +1,9 @@
+def WordCount(str)
+
+  "Argument goes here".scan(/[[:alpha:]]+/).count
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+WordCount(STDIN.gets)           
