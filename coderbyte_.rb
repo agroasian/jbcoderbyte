749df4a5f4c69bf3df231f0a25cd9179070240a0
agroasian <agro_asian@naver.com>
2015-07-31 15:00:28 +0900

    Create coderbyte_.rb

---
+++ coderbyte_.rb
@@ -0,0 +1,10 @@
+def SwapCase(str)
+
+  gun = str.swapcase
+  return gun
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SwapCase(STDIN.gets)
